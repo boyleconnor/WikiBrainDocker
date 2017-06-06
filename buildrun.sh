f947d1870fcf89954e6d336f80eb1c60f97d2bdd
Talha Ahsan <stwyde@gmail.com>
2017-06-06 13:58:36 -0500

    added a no cache tag
    no cache tag means it fully restarts from the beginning each time, which then means we can run images over and over again to test reproducibility.

--- buildrun.sh
+++ buildrun.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-docker build -t test .
+docker build --no-cache -t test .
 echo "build successful"
-#docker restart test
-#echo "restart successful"
-docker run --sysctl kernel.shmmax=64205988352 --sysctl kernel.shmall=15675290 -v /tmp/BonobosIncorporatedBusinessFiles:/output -it test
+docker run --sysctl kernel.shmmax=64205988352 --sysctl kernel.shmall=15675290 -v /tmp/BonobosIncorporatedBusinessFiles:/output --name outputFiles -it test
 echo "running shell succcesful"
