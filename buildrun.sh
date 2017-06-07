e7c18e1ad58a6eda63494a92b1eafa4e58649bfc
Talha Ahsan <stwyde@gmail.com>
2017-06-07 09:56:43 -0500

    updated buildrun, still gotta figure out volumes

--- buildrun.sh
+++ buildrun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build -t generatedata .
 echo "build successful"
-docker run --sysctl kernel.shmmax=64205988352 --sysctl kernel.shmall=15675290 -v /Public/generatedFiles:/output --name outputFiles -it generatedata
+docker run --sysctl kernel.shmmax=64205988352 --sysctl kernel.shmall=15675290 -v /Public/generatedFiles:/output -it generatedata
 echo "running shell succcesful"
