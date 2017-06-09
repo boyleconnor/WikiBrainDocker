4baf68c4f3e87301a941dbad1e1a38d57c3ed85d
Connor Boyle <connor.bo@gmail.com>
2017-06-09 14:23:37 -0500

    Fix run.sh to Sneakily Use Relative Path
    run.sh now sets up a volume using a relative path from the PWD
    environment variable. I'm slightly worried this won't work on all
    systems, but run.sh won't run on Windows in the first place anyway.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t generatedata .
-docker run --sysctl kernel.shmmax=64205988352 --sysctl kernel.shmall=15675290 -e WIKILANG=$1 -e MEM=9g -v /Users/sen/Documents/output:/output -it generatedata
+docker run --sysctl kernel.shmmax=64205988352 --sysctl kernel.shmall=15675290 -e WIKILANG=$1 -e MEM=9g -v $PWD/output:/output -it generatedata
