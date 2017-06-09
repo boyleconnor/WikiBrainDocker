5ef6efb936ed0883a9f0bbef0d035e3275dee36c
Connor Boyle <connor.bo@gmail.com>
2017-06-09 11:52:00 -0500

    Remove Misleading Message from buildrun.sh

--- buildrun.sh
+++ buildrun.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 docker build -t generatedata .
-echo "build successful"
 docker run --sysctl kernel.shmmax=64205988352 --sysctl kernel.shmall=15675290 -e WIKILANG=$1 -e MEM=9g -v /Users/sen/Documents/output:/output -it generatedata
