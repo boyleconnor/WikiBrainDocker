a3f2ec3f4758eb3a9d0376fad086c630410c8249
Connor Boyle <connor.bo@gmail.com>
2017-06-12 10:19:30 -0500

    Add Volume for Whole Wikibrain Directory

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t generatedata .
-docker run --sysctl kernel.shmmax=64205988352 --sysctl kernel.shmall=15675290 -e WIKILANG=$1 -e MEM=9g -v $PWD/output:/output generatedata
+docker run --sysctl kernel.shmmax=64205988352 --sysctl kernel.shmall=15675290 -e WIKILANG=$1 -e MEM=9g -v $PWD/wikibrain:/home/wikibrain -v $PWD/output:/output generatedata
