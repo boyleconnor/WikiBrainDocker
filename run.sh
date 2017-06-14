60968840ab7c08391ee54c613c30e80a704f0d92
Talha Ahsan <stwyde@gmail.com>
2017-06-14 15:45:29 -0500

    fixed spelling

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -rf output
 rm -rf host
-docker build -f DockerfileDevMode -t wikiBrainDocker .
-docker run --sysctl kernel.shmmax=64205988352 --sysctl kernel.shmall=15675290 -e WIKILANG=$1 -e MEM=9g -v $PWD/host:/host -v $PWD/output:/output wikiBrainDocker
+docker build -f DockerfileDevMode -t wikibraindocker .
+docker run --sysctl kernel.shmmax=64205988352 --sysctl kernel.shmall=15675290 -e WIKILANG=$1 -e MEM=9g -v $PWD/host:/host -v $PWD/output:/output wikibraindocker
