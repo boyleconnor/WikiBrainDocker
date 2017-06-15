c3e47d8eb26b170cbb406a63098edd9ca8e788ea
Talha Ahsan <stwyde@gmail.com>
2017-06-15 11:28:13 -0500

    fixed dockerfileWikiBrainOnly

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -rf output
 rm -rf host
-docker build -f DockerfileDevMode -t wikibraindocker .
+docker build -f DockerfileMaster -t wikibraindocker .
 docker run --sysctl kernel.shmmax=64205988352 --sysctl kernel.shmall=15675290 -e WIKILANG=$1 -e MEM=9g -v $PWD/host:/host -v $PWD/output:/output wikibraindocker
